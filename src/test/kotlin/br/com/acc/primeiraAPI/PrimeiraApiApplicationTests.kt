457318240bf65c5ce458b6908788aa19f301da99
Tiago Faustino <tiago.faustino@accenture.com>
2023-05-25 19:21:40 -0300

    Initial commit

---
+++ src/test/kotlin/br/com/acc/primeiraAPI/PrimeiraApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.acc.primeiraAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrimeiraApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
